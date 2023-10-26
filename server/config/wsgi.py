713b0f4990921504d837538a3ad155cfcdca95e6
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2023-10-26 12:26:09 +0000

    reset wsgi

--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -10,11 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# ModuleNotFoundError: No module named 'config' 문제 해결을 위해 sys에 경로 추가 
-import sys
-print("print! : ", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
